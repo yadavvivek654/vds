7c38a61f56a90f642bf89d68fea711b5ec2e3447
yadavvivek654 <35589459+yadavvivek654@users.noreply.github.com>
2026-01-03 22:50:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const images = [
+  "images/slide1.jpg",
+  "images/slide2.jpg",
+  "images/slide3.jpg"
+];
+
+let index = 0;
+const img = document.getElementById("carouselImage");
+
+function showSlide(){
+  img.style.opacity = 0;
+  setTimeout(() => {
+    img.src = images[index];
+    img.style.opacity = 1;
+  }, 400);
+}
+
+function nextSlide(){
+  index = (index + 1) % images.length;
+  showSlide();
+}
+
+function prevSlide(){
+  index = (index - 1 + images.length) % images.length;
+  showSlide();
+}
+
+// AUTO SLIDE
+setInterval(nextSlide, 5000);
